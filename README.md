41c9468d8584975dacdfaee12762f59e9c5602ef
prathgan <prathamgandhi.school@gmail.com>
2019-06-26 10:09:00 -0700

    update setup & README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Contract Operations Tool
+Lightweight Python package for converting Signal Temporal Logic into contracts for design-by-contract system design and doing operations on the contracts
--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-	name = 'package'
-	version = '0.1dev'
-	packages = []
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ContrOps",
+    version="0.0.1",
+    author="Pratham Gandhi",
+    author_email="prathamgandhi.school@gmail.com",
+    description="Lightweight Python package for converting Signal Temporal Logic into contracts for design-by-contract system design and doing operations on the contracts.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/prathgan/ContrOps",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
