ba5e7edf48845d6e44f85b2857e73b3d307bfa66
prathgan <prathamgandhi.school@gmail.com>
2019-06-28 15:48:14 -0700

    add list_union()

--- tool/utilities.py
+++ tool/utilities.py
@@ -6,6 +6,9 @@ def display_tree(root):
 	"""displays tree with root Node root in a readable manner"""
 	print(root)
 
+def list_union(l1,l2):
+	return list(set().union(l1,l2))
+
 def join_stringlists(str1, str2):
 	"""Return string of union of two sets represented as strings"""
 	return ','.join(list_union(str1.split(','),str2.split(',')))
