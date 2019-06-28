1f1277c4de9ff09413f9383bb282f81e60582942
prathgan <prathamgandhi.school@gmail.com>
2019-06-28 15:55:31 -0700

    add remove_dups_stringlist

--- tool/utilities.py
+++ tool/utilities.py
@@ -1,14 +1,21 @@
 def display_contract(c):
-	"""displays Contract c in a readable manner"""
+	"""Display Contract c in a readable manner"""
 	print(c)
 
 def display_tree(root):
-	"""displays tree with root Node root in a readable manner"""
+	"""Display tree with root Node root in a readable manner"""
 	print(root)
 
 def list_union(l1,l2):
+	"""Return union of elements in two lists"""
 	return list(set().union(l1,l2))
 
 def join_stringlists(str1, str2):
 	"""Return string of union of two sets represented as strings"""
 	return ','.join(list_union(str1.split(','),str2.split(',')))
+
+def remove_dups_stringlist(str):
+	"""Remove duplicates from list as string"""
+	arr = str.split(',')
+	arr = list(dict.fromkeys(arr))
+	return ','.join(arr)
