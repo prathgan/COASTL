0cdd958b803e5e1f10b7c9059a57026c58f19b8a
prathgan <prathamgandhi.school@gmail.com>
2019-06-28 13:10:43 -0700

    add stl funcs to contract.py

--- tool/contract.py
+++ tool/contract.py
@@ -1,9 +1,12 @@
+from stl import *
+
 class Contract(object):
 	"""docstring for Contract"""
-	def __init__(self, variables, assumptions, guarantees):
+	def __init__(self, variables, assumptions, guarantees)
 		self.process_variables(variables)
 		self.process_assumptions(assumptions)
 		self.process_guarantees(guarantees)
+		self.__isSat = 0
 
 	def process_variables(self, variables):
 		self.__variables = variables
@@ -25,6 +28,10 @@ class Contract(object):
 	@property
 	def guarantees(self):
 		return self.__guarantees
+
+	def saturate(self):
+		# A becomes A, and G becomes G or not A
+		pass
 
 	def __repr__(self):
 		print("Variables:")
