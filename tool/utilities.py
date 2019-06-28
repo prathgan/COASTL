303073a746dd9375d860b64192c173e36a01515b
prathgan <prathamgandhi.school@gmail.com>
2019-06-28 15:46:53 -0700

    add join_stringlists()

--- tool/utilities.py
+++ tool/utilities.py
@@ -3,4 +3,9 @@ def display_contract(c):
 	print(c)
 
 def display_tree(root):
+	"""displays tree with root Node root in a readable manner"""
 	print(root)
+
+def join_stringlists(str1, str2):
+	"""Return string of union of two sets represented as strings"""
+	return ','.join(list_union(str1.split(','),str2.split(',')))
