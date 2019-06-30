c2bcc36c689d623449b2079e4e86edca67318d1a
prathgan <prathamgandhi.school@gmail.com>
2019-06-30 12:40:01 -0700

    add tree processing conjunction

--- tool/operations.py
+++ tool/operations.py
@@ -1,3 +1,5 @@
+from contract import Contract
+
 def conjunction(contracts):
 	if len(contracts)>2:
 		return two_contract_conjunction(contracts[0],conjunction(contracts[1:]))
@@ -6,5 +8,5 @@ def conjunction(contracts):
 
 def two_contract_conjunction(c1,c2):
 	return Contract(list_union(c1.variables,c2.variables),
-		list_union(c1.assumptions,c2.assumptions),
-		list_union(c1.guarantees,c2.guarantees))
+		Node(None, [c1.assumptions,c2.assumptions], 0, "||", list_union(c1.assumptions.vars,c2.assumptions.vars), None, None, "||"),
+		Node(None, [c1.guarantees,c2.guarantees], 0, "||", list_union(c1.guarantees.vars,c2.guarantees.vars), None, None, "||"))
