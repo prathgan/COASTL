36702d71a8619f59f0b5e86ce956cc648a190c5f
prathgan <prathamgandhi.school@gmail.com>
2019-06-30 13:05:39 -0700

    add skeleton for composition

--- tool/operations.py
+++ tool/operations.py
@@ -15,3 +15,6 @@ def two_contract_conjunction(c1,c2):
 	return Contract(list_union(c1.variables,c2.variables),
 		Node(None, [c1.assumptions,c2.assumptions], 0, "||", list_union(c1.assumptions.vars,c2.assumptions.vars), None, None, "||"),
 		Node(None, [c1.guarantees,c2.guarantees], 0, "||", list_union(c1.guarantees.vars,c2.guarantees.vars), None, None, "||"))
+
+def composition(c1,c2):
+	return
