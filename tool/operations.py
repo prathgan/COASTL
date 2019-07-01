7f65b192d8d1147fcb8d1d15b666b49b4c513a92
prathgan <prathamgandhi.school@gmail.com>
2019-07-01 14:58:24 -0700

    add overloaded composition()

--- tool/operations.py
+++ tool/operations.py
@@ -20,7 +20,16 @@ def two_contract_conjunction(c1,c2):
 		Node(None, [c1.assumptions,c2.assumptions], 0, "|", list_union(c1.assumptions.vars,c2.assumptions.vars), None, None, "|"),
 		Node(None, [c1.guarantees,c2.guarantees], 0, "|", list_union(c1.guarantees.vars,c2.guarantees.vars), None, None, "|"))
 
+def composition(contracts):
+	if len(contracts)>2:
+		return two_contract_composition(contracts[0],composition(contracts[1:]))
+	else:
+		return two_contract_composition(contracts[0],contracts[1])
+
 def composition(c1,c2):
+	return two_contract_composition(c1,c2)
+
+def two_contract_composition(c1,c2):
 	if c1.isSat == 0:
 		c1.saturate()
 	if c2.isSat == 1:
