b72cd1c20958926cf5026d846a5a271794c9e6e4
prathgan <prathamgandhi.school@gmail.com>
2019-07-01 14:51:41 -0700

    add saturated check before operations

--- tool/operations.py
+++ tool/operations.py
@@ -12,11 +12,19 @@ def conjunction(c1,c2):
 	return two_contract_conjunction(c1,c2)
 
 def two_contract_conjunction(c1,c2):
+	if c1.isSat == 0:
+		c1.saturate()
+	if c2.isSat == 1:
+		c2.saturate()
 	return Contract(list_union(c1.variables,c2.variables),
 		Node(None, [c1.assumptions,c2.assumptions], 0, "|", list_union(c1.assumptions.vars,c2.assumptions.vars), None, None, "|"),
 		Node(None, [c1.guarantees,c2.guarantees], 0, "|", list_union(c1.guarantees.vars,c2.guarantees.vars), None, None, "|"))
 
 def composition(c1,c2):
+	if c1.isSat == 0:
+		c1.saturate()
+	if c2.isSat == 1:
+		c2.saturate()
 	return Contract(list_union(c1.variables,c2.variables),
 		Node(None, [c1.assumptions,c2.assumptions], 0, "|", list_union(c1.assumptions.vars,c2.assumptions.vars), None, None, "|"),
 		Node(None, [c1.guarantees,c2.guarantees], 0, "&", list_union(c1.guarantees.vars,c2.guarantees.vars), None, None, "&"))
