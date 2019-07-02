1898e0755b818f726ff532dd08fd3ff5e0e9f92b
prathgan <prathamgandhi.school@gmail.com>
2019-07-02 09:48:53 -0700

    fix not_guarantee syntax

--- tool/operations.py
+++ tool/operations.py
@@ -36,7 +36,7 @@ def two_contract_composition(c1,c2):
 	if c2.isSat == 1:
 		c2.saturate()
 	guarantee_new = Node(None, [c1.guarantees,c2.guarantees], 0, "&", list_union(c1.guarantees.vars,c2.guarantees.vars), None, None, "&")
-	not_guarantee = Node(None, [copy.deepcopy(guarantee_new)], 0, "~", guarantee_new.vars, None, None, "~"])
+	not_guarantee = Node(None, [copy.deepcopy(guarantee_new)], 0, "~", guarantee_new.vars, None, None, "~")
 	assumption_new_inter = Node(None, [c1.assumptions,c2.assumptions], 0, "&", list_union(c1.assumptions.vars,c2.assumptions.vars), None, None, "&")
 	assumption_new = Node(None, [assumption_new_inter,not_guarantee], 0, "|", list_union(assumption_new_inter.vars,not_guarantee.vars), None, None, "|")
 	return Contract(list_union(c1.variables,c2.variables),assumption_new,guarantee_new)
