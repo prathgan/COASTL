055c439d777d45bd09c1fbae18aa0a3124a9a3f5
prathgan <gandhips02@gmail.com>
2019-07-08 15:37:54 -0700

    begin globally

--- tool/constraints_notes.py
+++ tool/constraints_notes.py
@@ -12,3 +12,9 @@ b_inner_1 + b_inner_2 <= 2 * b_or
 # implies
 b_left + b_right >= b_implies
 2 * b_implies >= 1 - b_left + b_right
+
+# globally
+def g(start_t, end_t):
+    times = list(range(start_t, end_t))
+    for t in times:
+        
