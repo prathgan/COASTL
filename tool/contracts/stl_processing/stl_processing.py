340cfff1a845658d5a274a329255e279b75cdeec
prathgan <gandhips02@gmail.com>
2019-07-08 16:31:45 -0700

    Update stl_processing.py

--- tool/contracts/stl_processing/stl_processing.py
+++ tool/contracts/stl_processing/stl_processing.py
@@ -1,9 +1,7 @@
 import re
 from gurobipy import *
-from .stl_helper_funcs import *
-from .stl_helper_structures import SwitchDict
-from .stl_node import Node
-from .utilities.simple_utilities import remove_gurobi_log
+from .stl_parsing import parse_logic
+from .utilities.simple_utilities import remove_gurobi_log, parentheses_match
 
 def process(logic, remove_log=False):
 	"""
@@ -11,47 +9,10 @@ def process(logic, remove_log=False):
 	"""
 	if not parentheses_match(logic):
 		raise ValueError("Opening and closing brackets do not match, check '(' and ')'")
-	stl_tree = process_logic(logic, None, None)
+	stl_tree = parse_logic(logic, None, None)
 	m = Model("optimization_model")
 	# add constraints
 	# solve problem
 	if remove_log:
 		remove_gurobi_log()
-	return tree
-
-def process_logic(logic, range_start, range_end):
-	"""
-	Return root of tree structure which represents
-	a Signal Temporal Logic expression
-	"""
-	if logic=="":
-		return None
-	start, end = round_parens(logic)
-	if start==0 and end==len(logic)-1:
-		return process_logic(logic[1:len(logic)-1], range_start, range_end)
-	node_switch_0 = SwitchDict([("~",not_node),("G",g_node),("F",f_node)])
-	if node_switch_0[logic[0]] != None:
-		return node_switch_0[logic[0]](logic, start, end, range_start, range_end)
-	con_info = connector(logic)
-	if con_info != -1:
-		return con_node(con_info, range_start, range_end)
-	AP_info = predicate(logic, start, end)
-	if AP_info != -1:
-		return AP_node(AP_info, logic, range_start, range_end)
-
-def not_node(logic, start, end, range_start, range_end):
-	return Node(None, process_logic(logic[start+1:end, range_start, range_end], range_start, range_end), None, 0, "~", "", None, None, "~")
-
-def g_node(logic, start, end, range_start, range_end):
-	firstnum, secondnum, closep = square_parens(logic, 1)
-	return Node(None, process_logic(logic[closep+1:], firstnum, secondnum), None, 0, "G", "", None, None, logic[0:closep+1])
-
-def f_node(logic, start, end, range_start, range_end):
-	firstnum, secondnum, closep = square_parens(logic, 1)
-	return Node(None, process_logic(logic[closep+1:], firstnum, secondnum), None, 0, "F", "", firstnum, secondnum, logic[0:closep+1])
-
-def con_node(andor_info, range_start, range_end):
-	return Node(None, process_logic(andor_info[0], range_start, range_end), process_logic(andor_info[2], range_start, range_end), 0, andor_info[1], "", None, None, andor_info[1])
-
-def AP_node(AP_info,logic, range_start, range_end):
-	return Node(None, None, None, 1, AP_info[1], AP_info[0], range_start, range_end, logic)
+	return stl_tree
