cbf931e8e1193fb36827c11f99ae044920c86284
prathgan <gandhips02@gmail.com>
2019-07-08 16:31:58 -0700

    add parentheses_match

--- tool/contracts/stl_processing/utilities/simple_utilities.py
+++ tool/contracts/stl_processing/utilities/simple_utilities.py
@@ -17,5 +17,24 @@ def remove_dups_stringlist(str):
 def list_to_str(l):
 	return ','.join(l)
 
+def parentheses_match(string):
+	"""
+	Return True if opening and closing brackets are
+	all matched in an input logical expression
+	"""
+	verification_stack = []
+	matched = True
+	itr_index = 0
+	while itr_index < len(string) and matched:
+		if string[itr_index]=="(":
+			verification_stack.append(string[itr_index])
+		elif string[itr_index]==")":
+			if len(verification_stack)==0:
+				matched = False
+			else:
+				verification_stack.pop()
+		itr_index += 1
+	return matched and len(verification_stack) == 0
+
 def remove_gurobi_log():
 	os.remove("gurobi.log")
