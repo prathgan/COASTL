3299c3a574d81e0b44196f3b42fad6aebf38d476
prathgan <prathamgandhi.school@gmail.com>
2019-07-08 09:55:55 -0700

    replace process vars

--- tool/contracts/contract.py
+++ tool/contracts/contract.py
@@ -13,8 +13,8 @@ class Contract(object):
 			self.__guarantees = guarantees
 		else:
 			self.__guarantees = process(guarantees)
-		self.__variables = variables
-		# self.process_variables(variables)
+		# self.__variables = variables
+		self.process_variables(variables)
 		self.__isSat = 0
 
 	def process_assumptions(self, assumptions):
