9e60c75ab021a42f04feb54eacabbd8767b8a752
prathgan <gandhips02@gmail.com>
2019-07-08 11:44:21 -0700

    add constraints expressions notes

---
+++ constraints.py
@@ -0,0 +1,10 @@
+# not
+b_inner == 1 - b_not
+
+# and
+2 * b_and <= b_inner_1 + b_inner_2
+b_inner_1 + b_inner_2 <= 1 + b_and
+
+# or
+b_or <= b_inner_1 + b_inner_2
+b_inner_1 + b_inner_2 <= 2 * b_or
