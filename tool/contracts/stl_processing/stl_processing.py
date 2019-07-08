c31ec30efa1ecb0bd1bdcceb70d742ffe67ced3e
prathgan <prathamgandhi.school@gmail.com>
2019-07-08 11:28:39 -0700

    add downward propogation of range during construction

--- tool/contracts/stl_processing/stl_processing.py
+++ tool/contracts/stl_processing/stl_processing.py
@@ -9,9 +9,9 @@ def process(logic):
 	"""
 	if not parentheses_match(logic):
 		raise ValueError("Opening and closing brackets do not match, check '(' and ')'")
-	return process_logic(logic)
+	return process_logic(logic, None, None)
 
-def process_logic(logic):
+def process_logic(logic, range_start, range_end):
 	"""
 	Return root of tree structure which represents
 	a Signal Temporal Logic expression
@@ -20,31 +20,30 @@ def process_logic(logic):
 		return None
 	start, end = round_parens(logic)
 	if start==0 and end==len(logic)-1:
-		return process_logic(logic[1:len(logic)-1])
+		return process_logic(logic[1:len(logic)-1], range_start, range_end)
 	node_switch_0 = SwitchDict([("~",not_node),("G",g_node),("F",f_node)])
 	if node_switch_0[logic[0]] != None:
-		return node_switch_0[logic[0]](logic, start, end)
+		return node_switch_0[logic[0]](logic, start, end, range_start, range_end)
 	con_info = connector(logic)
 	if con_info != -1:
-		return con_node(con_info)
+		return con_node(con_info, range_start, range_end)
 	AP_info = predicate(logic, start, end)
 	if AP_info != -1:
-		return AP_node(AP_info, logic)
+		return AP_node(AP_info, logic, range_start, range_end)
 
-def not_node(logic, start, end):
-	return Node(None, process_logic(logic[start+1:end]), None, 0, "~", "", None, None, "~")
+def not_node(logic, start, end, range_start, range_end):
+	return Node(None, process_logic(logic[start+1:end, range_start, range_end], range_start, range_end), None, 0, "~", "", None, None, "~")
 
-def g_node(logic, start, end):
+def g_node(logic, start, end, range_start=None, range_end=None):
 	firstnum, secondnum, closep = square_parens(logic, 1)
-	return Node(None, process_logic(logic[closep+1:]), None, 0, "G", "", firstnum, secondnum, logic[0:closep+1])
+	return Node(None, process_logic(logic[closep+1:], firstnum, secondnum), None, 0, "G", "", None, None, logic[0:closep+1])
 
-def f_node(logic, start, end):
+def f_node(logic, start, end, range_start=None, range_end=None):
 	firstnum, secondnum, closep = square_parens(logic, 1)
-	return Node(None, process_logic(logic[closep+1:]), None, 0, "F", "", firstnum, secondnum, logic[0:closep+1])
+	return Node(None, process_logic(logic[closep+1:], firstnum, secondnum), None, 0, "F", "", firstnum, secondnum, logic[0:closep+1])
 
-def con_node(andor_info):
-	return Node(None, process_logic(andor_info[0]), process_logic(andor_info[2]), 0, andor_info[1], "", None, None, andor_info[1])
+def con_node(andor_info, range_start, range_end):
+	return Node(None, process_logic(andor_info[0], range_start, range_end), process_logic(andor_info[2], range_start, range_end), 0, andor_info[1], "", None, None, andor_info[1])
 
-#UNFINISHED - AP nodes need range from previous nodes (see notes)
-def AP_node(AP_info,logic):
-	return Node(None, None, None, 1, AP_info[1], AP_info[0], None, None, logic)
+def AP_node(AP_info,logic, range_start, range_end):
+	return Node(None, None, None, 1, AP_info[1], AP_info[0], range_start, range_end, logic)
