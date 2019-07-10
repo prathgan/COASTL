b4ed676285b4e6239169c82a113ca49c71ccb879
prathgan <gandhips02@gmail.com>
2019-07-09 22:32:15 -0700

    Update contract.py

--- tool/contracts/contract.py
+++ tool/contracts/contract.py
@@ -17,22 +17,10 @@ class Contract(object):
 		self.__isSat = 0
 
 	def process_assumptions(self, assumptions):
-		assum_arr = []
-		var_str = ""
-		for assumption in assumptions:
-			temp_root = process(assumption)
-			assum_arr.append(temp_root)
-			var_str += temp_root.vars
-		self.__assumptions = process
+		self.__assumptions = parse(assumptions)
 
 	def process_guarantees(self, guarantees):
-		guar_arr = []
-		var_str = ""
-		for guarantee in guarantees:
-			temp_root = process(guarantee)
-			guar_arr.append(temp_root)
-			var_str += temp_root.vars
-		self.__guarantees = Node(None, guar_arr, 0, "&", remove_dups_stringlist(var_str), None, None, "&")
+		self.__guarantees = parse(guarantees)
 
 	def process_variables(self, variables):
 		for var in variables:
