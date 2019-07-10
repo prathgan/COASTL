6935839dedb15e008a7f88a9f24bc9df0b6aa845
prathgan <gandhips02@gmail.com>
2019-07-10 11:04:48 -0700

    add topmost node constraint b0==1

--- tool/contracts/stl_processing/stl_constraints.py
+++ tool/contracts/stl_processing/stl_constraints.py
@@ -3,6 +3,7 @@ from gurobipy import *
 def create_constraints(tree, m=None):
     if m == None:
         m = Model("solver")
+    m = topmost_constr(node, m)
     node_switch = SwitchDict([("~",not_constr),
                               ("G",g_constr),
                               ("F",f_constr),
@@ -13,6 +14,13 @@ def create_constraints(tree, m=None):
         m = create_constraints(tree.child1, m)
     if tree.child2 is not None:
         m = create_constraints(tree.child2, m)
+    return m
+
+def topmost_constr(node, m):
+    if node.parent is None:
+        bin_name = "b_"+node.string_rep
+        exec(bin_name+"= m.addVar(vtype=GRB.BINARY, name='b_"+node.string_rep+"')")
+        exec("m.addConstr("+bin_name+" == 1, 'c_"+bin_name+"')")
     return m
 
 def g_constr(node, m):
