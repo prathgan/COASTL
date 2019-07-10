0852c0a08559d0eeb74778c650dbf90094abb90e
prathgan <gandhips02@gmail.com>
2019-07-10 10:07:01 -0700

    add recursive structure

--- tool/contracts/stl_processing/stl_constraints.py
+++ tool/contracts/stl_processing/stl_constraints.py
@@ -3,4 +3,9 @@ from gurobipy import *
 def create_constraints(tree, m=None):
     if m == None:
         m = Model("solver")
-    m = create_constraints()
+    
+    if tree.child1 is not None:
+        m = create_constraints(tree.child1, m)
+    if tree.child2 is not None:
+        m = create_constraints(tree.child2, m)
+    return m
