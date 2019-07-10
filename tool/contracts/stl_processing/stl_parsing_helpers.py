e68ec2533ed12c154b01b693ad4f4951c81d675d
prathgan <gandhips02@gmail.com>
2019-07-10 10:02:00 -0700

    add SwitchDict

--- tool/contracts/stl_processing/stl_parsing_helpers.py
+++ tool/contracts/stl_processing/stl_parsing_helpers.py
@@ -80,3 +80,7 @@ def predicate_info(logic, start, equals_ind):
 		operator = var[-1] + operator
 		var = var[:-1]
 	return var, operator
+
+class SwitchDict(dict):
+    def __getitem__(self, key):
+        return dict.get(self, key)
