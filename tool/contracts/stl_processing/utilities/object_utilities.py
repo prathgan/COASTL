d6d3e92b649c788ec9e41b01c55bf00c01087d2b
prathgan <gandhips02@gmail.com>
2019-07-12 14:10:37 -0700

    add display_model

--- tool/contracts/stl_processing/utilities/object_utilities.py
+++ tool/contracts/stl_processing/utilities/object_utilities.py
@@ -5,3 +5,7 @@ def display_contract(c):
 def display_tree(root):
 	"""Display tree with root Node root in a readable manner"""
 	print(root)
+
+def display_model(m):
+	for v in m.getVars():
+	    print('%s %g' % (v.varName, v.x))
