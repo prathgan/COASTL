ad93ef04fb48c6cb6eae4d5266fc9afe4d885ea3
prathgan <gandhips02@gmail.com>
2019-07-15 14:03:21 -0700

    fix operation method overloading

--- STLContractTool/contracts/operations.py
+++ STLContractTool/contracts/operations.py
@@ -3,14 +3,14 @@ from .stl_processing.utilities.simple_utilities import *
 from .stl_processing.stl_node import Node
 import copy
 
-def conjunction(contracts):
-	if len(contracts)>2:
-		return two_contract_conjunction(contracts[0],conjunction(contracts[1:]))
+def conjunction(c1,c2=None):
+	if c2 is not None:
+		return two_contract_conjunction(c1,c2)
 	else:
-		return two_contract_conjunction(contracts[0],contracts[1])
-
-def conjunction(c1,c2):
-	return two_contract_conjunction(c1,c2)
+		if len(c1)>2:
+			return two_contract_conjunction(c1[0],conjunction(c1[1:]))
+		else:
+			return two_contract_conjunction(c1[0],c1[1])
 
 def two_contract_conjunction(c1,c2):
 	if c1.isSat == 0:
@@ -21,14 +21,14 @@ def two_contract_conjunction(c1,c2):
 		Node(None, c1.assumptions, c2.assumptions, 0, "||", list_union(c1.assumptions.vars,c2.assumptions.vars), None, None, "||"),
 		Node(None, c1.guarantees, c2.guarantees, 0, "&&", list_union(c1.guarantees.vars,c2.guarantees.vars), None, None, "&&"))
 
-def composition(contracts):
-	if len(contracts)>2:
-		return two_contract_composition(contracts[0],composition(contracts[1:]))
+def composition(c1, c2=None):
+	if c2 is not None:
+		return two_contract_composition(c1,c2)
 	else:
-		return two_contract_composition(contracts[0],contracts[1])
-
-def composition(c1,c2):
-	return two_contract_composition(c1,c2)
+		if len(c1)>2:
+			return two_contract_composition(c1[0],composition(c1[1:]))
+		else:
+			return two_contract_composition(c1[0],c1[1])
 
 def two_contract_composition(c1,c2):
 	if c1.isSat == 0:
