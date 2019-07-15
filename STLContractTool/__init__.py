22445b1e4502540c399c234d5a1662f331a1e930
prathgan <gandhips02@gmail.com>
2019-07-15 10:01:34 -0700

    rename src folder

---
+++ STLContractTool/__init__.py
@@ -0,0 +1,12 @@
+from .contracts.contract import Contract
+from .contracts.operations import *
+
+from .contracts.stl_processing.stl_processing import *
+from .contracts.stl_processing.stl_parsing import *
+from .contracts.stl_processing.stl_parsing_helpers import *
+from .contracts.stl_processing.stl_node import *
+from .contracts.stl_processing.stl_constraints import *
+from .contracts.stl_processing.stl_constraints_helpers import *
+
+from .contracts.stl_processing.utilities.object_utilities import *
+from .contracts.stl_processing.utilities.simple_utilities import *
--- STLContractTool/test.py
+++ STLContractTool/test.py
@@ -10,6 +10,12 @@ from timeit import default_timer as timer
 # from sympy import S
 from contracts.contract import *
 from contracts.operations import *
+from contracts.stl_processing.stl_constraints import *
+from contracts.stl_processing.stl_constraints_helpers import *
+from contracts.stl_processing.utilities.object_utilities import *
+import sympy
+from gurobipy import *
+
 
 """
 timing toolkit:
@@ -18,15 +24,26 @@ end = timer()
 print(end - start)
 """
 
-# tree = process("(F[0,20]((5<=x(1))&&(x(1)<=5)&&(0<=x(2))&&(x(2)<=0)))U(F[0,20]((5<=x(1))&&(x(1)<=5)&&(0<=x(2))&&(x(2)<=0)))")
-# print(tree.child1.child1.child2.child2.child1)
-# print(tree.child1.child1.child2.child2.child1.range_end)
+tree = process("F[0,10]((~(x<=1))&&(x<=10))")
+# tree = process("~(x<=2)")
+m = create_constraints(tree, remove_log=True)
+# print(m.getConstrs())
+m.optimize()
+display_model(m)
 
-#c1 = Contract(["x","y"],"(x(1)<=10)&((y<=20))","~(~(G[0,10](F[1,3](~(x>=1)&(y<=0)))))")
-#c2 = Contract(["x","y"],"(x(1)<=10)&((y<=20))","~(~(G[0,10](F[1,3](~(x>=1)&(y<=0)))))")
-#conjunction(c1,c2)
+# print(isolate_0(process("(x<=1)")))
+
+#tree = process("(F[0,20]((5<=x(1))&&(x(1)<=5)&&(0<=x(2))&&(x(2)<=0)))U(F[0,20]((5<=x(1))&&(x(1)<=5)&&(0<=x(2))&&(x(2)<=0)))")
+#print(tree.vars)
+# remove_gurobi_log()
+# print(tree.child1.child1.child2.child2.child1)
+# print(tree.child1.child1.child2.child2.child1.parent)
+
+# c1 = Contract(["x","y"],"(x(1)<=10)&&((y<=20))","~(~(G[0,10](~(x>=1)&&(y<=0))))")
+# c2 = Contract(["x","y"],"(x(1)<=10)&&((y<=20))","F[1,3]((~(x>=1))&&(y<=0))")
+# conjunction(c1,c2)
 #print(end - start)
-#composition(c1,c2)
+# composition(c1,c2)
 #print(end - start)
 
 # start, end = round_parens("x<=10")
