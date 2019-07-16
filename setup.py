ee3331308bef1a1ac79fe68f8ca54e89c03976c0
prathgan <gandhips02@gmail.com>
2019-07-16 09:41:43 -0700

    change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="contract-ops",
+    name="coastl",
     version="0.0.1",
     author="Pratham Gandhi",
     author_email="prathamgandhi.school@gmail.com",
