1370595fa25b2959398ae7793689dfea8168c968
prathgan <gandhips02@gmail.com>
2019-07-19 22:57:38 -0700

    Update stl.py

---
+++ examples/stl.py
@@ -0,0 +1,7 @@
+from STLContractTool import parse_stl, create_model_stl, synthesize_stl
+
+stl_tree = parse_stl("G[0,10]((x<=10)&&(~(x<=5)))")
+m = create_model_stl(stl_tree)
+m = synthesize_stl(m)
+solved_vars = m.getVars()
+print(solved_vars)
