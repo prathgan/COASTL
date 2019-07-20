02390bf6d74f2c153cc37780fc1958a08dfbfc33
prathgan <gandhips02@gmail.com>
2019-07-19 22:57:14 -0700

    add stl_toolkit imports

--- coastl/__init__.py
+++ coastl/__init__.py
@@ -10,3 +10,12 @@ from .contracts.stl_processing.stl_constraints_helpers import *
 
 from .contracts.stl_processing.utilities.object_utilities import *
 from .contracts.stl_processing.utilities.simple_utilities import *
+
+from .stl_toolkit
+
+from .stl_toolkit.stl_processing import *
+from .stl_toolkit.stl_parsing import *
+from .stl_toolkit.stl_parsing_helpers import *
+from .stl_toolkit.stl_node import *
+from .stl_toolkit.stl_constraints import *
+from .stl_toolkit.stl_constraints_helpers import *
