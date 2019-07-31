15c514dff0fc72eee7534a110697014a1a2f3757
prathgan <gandhips02@gmail.com>
2019-07-31 15:45:34 -0700

    remove redundant imports

--- coastl/__init__.py
+++ coastl/__init__.py
@@ -1,21 +1,12 @@
 from .contracts.contract import Contract
 from .contracts.operations import *
 
-from .contracts.stl_processing.stl_processing import *
-from .contracts.stl_processing.stl_parsing import *
-from .contracts.stl_processing.stl_parsing_helpers import *
-from .contracts.stl_processing.stl_node import *
-from .contracts.stl_processing.stl_constraints import *
-from .contracts.stl_processing.stl_constraints_helpers import *
-
-from .contracts.stl_processing.utilities.object_utilities import *
-from .contracts.stl_processing.utilities.simple_utilities import *
-
-from .stl_toolkit
-
 from .stl_toolkit.stl_processing import *
 from .stl_toolkit.stl_parsing import *
 from .stl_toolkit.stl_parsing_helpers import *
 from .stl_toolkit.stl_node import *
 from .stl_toolkit.stl_constraints import *
 from .stl_toolkit.stl_constraints_helpers import *
+
+from .stl_toolkit.utilities.object_utilities import *
+from .stl_toolkit.utilities.simple_utilities import *
